2ea2a82b6329afed71e88fe887dd71fbbb9155a3
Михаил Атапович <Atapovich@unibel.by>
2025-05-22 10:45:51 +0300

    final

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 from datetime import timedelta, datetime, timezone
 import os
 import cloudscraper
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
 
 tracked_teams = {"faze", "falcons", "spirit"}
 HLTV_URL = "https://www.hltv.org/matches"
-
+keyboard = [
+        [InlineKeyboardButton("Показать матчи", callback_data="matches")],
+        [InlineKeyboardButton("Список команд", callback_data="list")]
+    ]
+reply_markup = InlineKeyboardMarkup(keyboard)
 
 def fetch_matches(teams):
     headers = {
@@ -20,20 +24,19 @@ def fetch_matches(teams):
         "Referer": "https://www.hltv.org/",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Cache-Control": "max-age=0",
-        "Cookie": "__cflb=0H28vvhCcx7neqKqegLg3bDFqvDd36FWtMXV4585HVW; MatchFilter={%22active%22:false%2C%22live%22:false%2C%22stars%22:1%2C%22lan%22:false%2C%22teams%22:[]}; cf_clearance=tB6MRjsDAv6.B7IsrAUqVON_zt6ZiKESwxE0ajPgaww-1747830360-1.2.1.1-kM0706qWJHowoaRpgpvgYdhwZ1VqjMDF2vfQ5PsvFK.tE1TINndz0wOa0kVmGBOHk4bVG8OIVNPs0bXxQmbpnH7Bc9COybszEPyGczpa8A7RZ5_T2o2zfBDQLFZNhQtqTnT4BKQpJYJpYHQBhDVm7CTPvBRqcEXaN3HTkpTOyN_O__SVUmfiwdUCSW5r8DUD08_Jd5JzFNhu2Vw8o4MbJWc.ztC2E.TVlehLCZRU6XDxO4dO8FMs_kY51nWG1aMJ5rpR4OuvyMljuLON_C2IezKne5dGID7Bif4Zvh9VEabpkeK7I_utz0TrbE9GuiLJ2i0P4KIPfsA3QJZMNwbOUdcoam3Ey4puwwrzgE36jf4; CookieConsent={stamp:%278F3Qk+9gom759VEQKiFKB+DftznsdMGhPXi01vgm66AMiBdB4Kv+/A==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1747830363198%2Cregion:%27by%27}; _fbp=fb.1.1747830395231.38489471453328275; _sharedid=4908594a-3963-4eee-a54f-c6334fe2066e; _sharedid_cst=zix7LPQsHA%3D%3D; _lr_env_src_ats=false; _ga_525WEYQTV9=GS2.1.s1747830398$o1$g0$t1747830398$j60$l0$h0$dXSv_Xwwl_b_bLcM-HmFoeqr0vDnadiguLw; _ga=GA1.1.438590886.1747830399; __gads=ID=dd941af737606c91:T=1747830372:RT=1747830372:S=ALNI_MbURAdAIptqg2RUcfJCTKnZ6sGcFw; __gpi=UID=000010c9b8d2b501:T=1747830372:RT=1747830372:S=ALNI_MZhq62yo6vbnUOibgJLk15vN4R7EQ; __eoi=ID=c9f75795bd1f4eb7:T=1747830372:RT=1747830372:S=AA-AfjaS3-P5rVYAHLHmkyIHdTLf; cto_bundle=EpyRTl9DWUdrJTJGQyUyQlhzcWhialZubk9UR1NnMGdnMXlUQXRJVU1WMnVRc0dyNXJUWkVIRm1HUlozNFE3VDlDclRJQk5TOUtyMWlmZHhxNXhiZHdvWHcxS0Y0TlFRNE54YmYlMkZUUGVQSDRtOU5WVmdhQyUyQlBjUjB2Z0hnY3ZySnc5WTJXSEFFMjBQa2ZRTEZJWkVIeDBCZEVHWXM2dyUzRCUzRA; __cf_bm=J6lFNqlp1jAc6QVAvNAQoCnn8KlQdYxlAgHIv1VeHCA-1747834644-1.0.1.1-yP6Tg9_.RNHJhhYWoq79Rg3okL61NKQxPegcEkwgj81Rzuf2RLNs0TqzTxSgFJbBDFiXNgJ.BwYbFx_.eEiAd1zJyWxa07hAVZPSyDkzX4U",
         "Priority": "u=0, i"
 }
     print("Fetching matches...")
     scraper = cloudscraper.create_scraper()
     response = scraper.get(HLTV_URL)
-    print(response)
+    if response.status_code == 403:
+        return ["Матчи не были загружены, попробуйте позже."]
     soup = BeautifulSoup(response.text, "html.parser")
 
     now = datetime.now(timezone.utc)
     matches = []
 
-    # Находим все блоки с матчами по классу "match-bottom"
-    match_blocks = soup.select("div.match")
+    match_blocks = soup.select("div.matches-chronologically.matches-chronologically-hide div.match-bottom")
     print(match_blocks)
     for block in match_blocks:
         try:
@@ -51,31 +54,78 @@ def fetch_matches(teams):
             team1 = team1_elem.text.strip().lower()
             team2 = team2_elem.text.strip().lower()
 
-            if any(t in team1 or t in team2 for t in teams):
+            if any(t == team1 or t == team2 for t in teams):
                 matches.append(f"{(match_time + timedelta(hours=3)).strftime('%d.%m %H:%M UTC')} — {team1.title()} vs {team2.title()}")
         except Exception as e:
             print(f"Ошибка при парсинге матча: {e}")
             continue
     return matches
 
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "matches":
+        matches_list = fetch_matches(tracked_teams)
+        text = "Ближайшие матчи:\n\n" + "\n".join(matches_list) + "\n\n" + "Для просмотра моих возможностей напишите /start" if matches_list else "В ближайшие 24 часа матчей отслеживаемых команд нет."+ "\n\n" + "Для просмотра моих возможностей напишите /start"
+        await query.edit_message_text(text, reply_markup=reply_markup)
+    elif query.data == "list":
+        if tracked_teams:
+            await query.edit_message_text("Отслеживаемые команды:\n" + "\n".join(sorted(tracked_teams)) + "\n\n" + "Для просмотра моих возможностей напишите /start", reply_markup=reply_markup)
+        else:
+            await query.edit_message_text("Пока что ни одна команда не отслеживается."+ "\n\n" + "Для просмотра моих возможностей напишите /start", reply_markup = reply_markup)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я отслеживаю матчи. Напиши /matches чтобы узнать ближайшие игры.")
+
+    await update.message.reply_text(
+        "Привет! Я отслеживаю матчи.\nНажми кнопку ниже или используй команды:\n/matches — ближайшие игры\n/add <команда> — добавить команду\n/list — список команд\n/remove <команда> — удалить команду",
+        reply_markup=reply_markup
+    )
 
 async def matches(update: Update, context: ContextTypes.DEFAULT_TYPE):
     matches = fetch_matches(tracked_teams)
     if matches:
-        await update.message.reply_text("Ближайшие матчи:\n\n" + "\n".join(matches))
+        await update.message.reply_text("Ближайшие матчи:\n\n" + "\n".join(matches) + "\n\n" + "Для просмотра моих возможностей напишите /start", reply_markup = reply_markup)
     else:
-        await update.message.reply_text("В ближайшие 24 часа матчей отслеживаемых команд нет.")
+        await update.message.reply_text("В ближайшие 24 часа матчей отслеживаемых команд нет.\n\n"+ "\n\n" + "Для просмотра моих возможностей напишите /start", reply_markup = reply_markup)
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
         await update.message.reply_text("Используй: /add <команда>")
         return
-    team = context.args[0].lower()
-    tracked_teams.add(team)
-    await update.message.reply_text(f"Команда '{team}' добавлена!")
+    team = ""
+    for arg in context.args:
+        if arg == context.args[0]:
+            team = context.args[0].lower()
+        else:
+            team = team + " " + (arg.lower())
+    if team in tracked_teams:
+        await update.message.reply_text(f"Команда '{team}' уже в списке отслеживаемых.", reply_markup=reply_markup)
+    else:
+        tracked_teams.add(team)
+        await update.message.reply_text(f"Команда '{team}' добавлена!", reply_markup=reply_markup)
+
+async def remove(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        if tracked_teams:
+            await update.message.reply_text("Отслеживаемые команды:\n" + "\n".join(
+                sorted(tracked_teams)) + "\n\n" + "Для удаления команды напишите /remove <название команды>")
+        else:
+            await update.message.reply_text(
+                "Пока что ни одна команда не отслеживается." + "\n\n" + "Для просмотра моих возможностей напишите /start", reply_markup = reply_markup)
+    else:
+        team = ""
+        for arg in context.args:
+            if arg == context.args[0]:
+                team = context.args[0].lower()
+            else:
+                team = team + " " + (arg.lower())
+        if team in tracked_teams:
+            tracked_teams.discard(team)
+            await update.message.reply_text(f"Команда '{team}' была удалена!", reply_markup=reply_markup)
+        else:
+            await update.message.reply_text(f"Команда '{team}' не была найдена." + "\n\n" + "Для удаления команды напишите /remove <название команды>", reply_markup=reply_markup)
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -91,10 +141,12 @@ def run_server():
 def main():
     threading.Thread(target=run_server, daemon=True).start()
 
-    app = ApplicationBuilder().token(os.getenv("YOUR_TELEGRAM_BOT_TOKEN")).build()
+    app = ApplicationBuilder().token("token").build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("matches", matches))
     app.add_handler(CommandHandler("add", add))
+    app.add_handler(CommandHandler("remove", remove))
+    app.add_handler(CallbackQueryHandler(button_handler))
     app.run_polling()
 
 if __name__ == "__main__":
